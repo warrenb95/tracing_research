f7901d8aaebe42b96bed3e81467e26bc9540fd37
Warren Brown <wa.brown@reply.com>
2021-10-21 08:54:57 +0100

    updates

---
+++ internal/tracer/jaeger.go
@@ -0,0 +1,45 @@
+package tracer
+
+import (
+	"fmt"
+	"io"
+	"log"
+
+	"github.com/opentracing/opentracing-go"
+	"github.com/uber/jaeger-client-go"
+	jaegercfg "github.com/uber/jaeger-client-go/config"
+	jaegerlog "github.com/uber/jaeger-client-go/log"
+
+	"github.com/uber/jaeger-lib/metrics"
+)
+
+func Create(serviceName string) (opentracing.Tracer, io.Closer, error) {
+	cfg := jaegercfg.Configuration{
+		ServiceName: serviceName,
+		Sampler: &jaegercfg.SamplerConfig{
+			Type:  jaeger.SamplerTypeConst,
+			Param: 1,
+		},
+		Reporter: &jaegercfg.ReporterConfig{
+			LogSpans: true,
+		},
+	}
+
+	// Example logger and metrics factory. Use github.com/uber/jaeger-client-go/log
+	// and github.com/uber/jaeger-lib/metrics respectively to bind to real logging and metrics
+	// frameworks.
+	jLogger := jaegerlog.StdLogger
+	jMetricsFactory := metrics.NullFactory
+
+	// Initialize tracer with a logger and a metrics factory
+	tracer, closer, err := cfg.NewTracer(
+		jaegercfg.Logger(jLogger),
+		jaegercfg.Metrics(jMetricsFactory),
+	)
+	if err != nil {
+		log.Fatalf("can't create new tracer, %v", err)
+		return nil, nil, fmt.Errorf("can't create new tracer, %v", err)
+	}
+
+	return tracer, closer, nil
+}
